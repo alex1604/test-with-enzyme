ea24f201c2dc58491b29514b2a033a1357af0217
Alejandro <92.alejandrogarcia@gmail.com>
2018-06-06 12:05:36 +0200

    first commit

---
+++ src/Calculator.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+/*class OnOff extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { on: false };
+	}
+	render() {
+		return (
+			<div>
+				<button onClick={this.handleClick}>Turn {this.state.on ? 'off' : 'on'}</button>
+			</div>
+		);
+	}
+	handleClick = event => {
+		this.setState({ on: !this.state.on });
+	}
+}*/
+
+export default Calculator;
---
+++ src/__tests__/calculator.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
+import Calculator from '../Calculator.js';
+
+// <div> <button onClick=?>Turn on</button> </div>
+// initialState: { on: false }
+// klickfunktion, state
+/*describe('OnOff component test suite', () => {
+
+	it('renders without crashing', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(<OnOff />, div);
+		ReactDOM.unmountComponentAtNode(div);
+	})
+	test('shallow smoke test', () => {
+		let wrapper = shallow(<OnOff />);
+	})
+
+	test('initial state should be false', () => {
+		let wrapper = shallow(<OnOff />);
+		expect( wrapper.state('on') ).toBe( false );
+	})
+
+	test('state from false to true when clicked', () => {
+		let wrapper = shallow(<OnOff />);
+		let button = wrapper.find('button').at(0);
+		button.simulate('click');
+		expect( wrapper.state('on') ).toBe( true );
+	})
+	test('state from true to false when clicked', () => {
+		let wrapper = shallow(<OnOff />);
+		wrapper.setState({ on: true });
+		let button = wrapper.find('button').at(0);
+		button.simulate('click');
+		expect( wrapper.state('on') ).toBe( false );
+	})
+
+
+})*/
