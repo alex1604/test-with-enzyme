2d0028c245108c1ed851804176175fc6e57e3333
Alejandro <92.alejandrogarcia@gmail.com>
2018-06-08 19:52:55 +0200

    tests for calculator finished successfully - 13 test cases

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,20 +1,57 @@
 import React, {Component} from 'react';
 
-/*class OnOff extends Component {
+/*
+Komponenten ska innehålla ett textfält och två button-element. Den ska dessutom skriva ut 
+ett tal. Talet ska finnas lagrat i komponentens state. Användaren ska kunna skriva in strängar
+i textfältet. När man klickar på den första knappen ska värdet i state ökas; den andra 
+knappen minskar värdet.
+Om användaren skriver in ett tal i textfältet så ska det ersätta talet som finns lagrat i 
+state. Men om användaren skriver in något som inte går att göra om till ett tal så ska värdet
+behållas.*/
+
+
+class Calculator extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { on: false };
+		this.state = {
+			number: 0
+		};
 	}
 	render() {
 		return (
 			<div>
-				<button onClick={this.handleClick}>Turn {this.state.on ? 'off' : 'on'}</button>
+				Type something:<input type="text" id="input" placeholder="Type a number or some text" onKeyPress={this.handleInput}/>
+				<div id="buttons">
+					<button onClick={this.increase} type="button" id="increase">Increase</button>
+					<button onClick={this.decrease} type="button" id="decrease">Decrease</button>
+					<button onClick={()=>{this.setState({number: 0})}} type="button" id="clear">Clear</button>
+				</div>
+				Value: {this.state.number}
 			</div>
 		);
 	}
-	handleClick = event => {
-		this.setState({ on: !this.state.on });
+	increase = event =>{
+		this.setState({number: this.state.number + 1, click: true})
 	}
-}*/
+	decrease = event =>{
+		this.setState({number: this.state.number - 1, click: true})
+
+	}
+	handleInput = event => {
+		console.log(event.key)
+		if (!isNaN(Number(event.key)) && Number(event.key) !== Infinity){
+			if(this.state.number === 0){
+				this.setState({number: Number(event.key), click: false})	
+			} else if (!this.state.click){
+				let prev = String(this.state.number);
+				let next = prev + event.key;
+				this.setState({number: Number(next), click: false})
+			} else {
+				this.setState({number: event.key, click: false})
+			}
+		}
+	}
+	
+}
 
 export default Calculator;
--- src/__tests__/calculator.test.js
+++ src/__tests__/calculator.test.js
@@ -1,40 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {shallow} from 'enzyme';
+import {shallow, mount, render} from 'enzyme';
 import Calculator from '../Calculator.js';
 
 // <div> <button onClick=?>Turn on</button> </div>
 // initialState: { on: false }
 // klickfunktion, state
-/*describe('OnOff component test suite', () => {
-
+describe('Calculator component test suite', () => {
+	let wrapper = shallow(<Calculator />);
 	it('renders without crashing', () => {
 		const div = document.createElement('div');
-		ReactDOM.render(<OnOff />, div);
+		ReactDOM.render(<Calculator />, div);
 		ReactDOM.unmountComponentAtNode(div);
 	})
 	test('shallow smoke test', () => {
-		let wrapper = shallow(<OnOff />);
+		wrapper;
 	})
 
-	test('initial state should be false', () => {
-		let wrapper = shallow(<OnOff />);
-		expect( wrapper.state('on') ).toBe( false );
+	test('initial state should be 0', () => {
+		expect( wrapper.state('number') ).toBe( 0 );
 	})
 
-	test('state from false to true when clicked', () => {
-		let wrapper = shallow(<OnOff />);
+	test('state increase by one when click on increase button', () => {
 		let button = wrapper.find('button').at(0);
 		button.simulate('click');
-		expect( wrapper.state('on') ).toBe( true );
+		expect( wrapper.state('number') ).toBe( 1 );
 	})
-	test('state from true to false when clicked', () => {
-		let wrapper = shallow(<OnOff />);
-		wrapper.setState({ on: true });
-		let button = wrapper.find('button').at(0);
+	test('state decrease by one when click on decrease button', () => {
+		let button = wrapper.find('button').at(1);
 		button.simulate('click');
-		expect( wrapper.state('on') ).toBe( false );
+		expect( wrapper.state('number') ).toBe( 0 );
 	})
+	test('state remains the same when text is typed in input field', () => {
+		let input = wrapper.find('input');
+		input.simulate('keyPress', {key: 'a'});
+		expect( wrapper.state('number') ).toBe( wrapper.state('number') );
+		
+	})
+	test('state is replaced when number is typed in input field', () => {
+		let input = wrapper.find('input');
+		input.simulate('keyPress', {key: '5'});
+		expect( wrapper.state('number') ).toBe( 5 );
+		
+	})
+	test('new number input is added to existing number in input field and state is replaced', () => {
 
+		let input = wrapper.find('input');
+		input.simulate('keyPress', {key: '5'});
+		expect( wrapper.state('number') ).toBe( 55 );
+		
+	})
+	test('if undefined in input field then nothing happens ', () => {
+		let input = wrapper.find('input');
+		input.simulate('keyPress', {key: 'undefined'});
+		expect( wrapper.state('number') ).toBe( wrapper.state('number') );
+		
+	})
+	test('if empty string in input field then nothing happens ', () => {
+		let input = wrapper.find('input');
+		input.simulate('keyPress', {key: ''});
+		expect( wrapper.state('number') ).toBe( wrapper.state('number') );
+		
+	})
+	test('if null in input field then nothing happens ', () => {
+		let input = wrapper.find('input');
+		input.simulate('keyPress', {key: 'null'});
+		expect( wrapper.state('number') ).toBe( wrapper.state('number') );
+		
+	})
+	test('if string Infinity in input field then nothing happens ', () => {
+		let input = wrapper.find('input');
+		input.simulate('keyPress', {key: 'Infinity'});
+		expect( wrapper.state('number') ).toBe( wrapper.state('number') );
+		
+	})
+})
 
-})*/
